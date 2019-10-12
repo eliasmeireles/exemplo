70b2320ab656a1ef04e87f4065f4b956b74b348d
Elias Meireles <eliasmflilico@gmail.com>
2019-10-12 16:58:05 -0300

    Example of Kotlin native for Android and Ios shared code.

---
+++ AndroidIOSKotlinNative/SharedCode/src/androidMain/kotlin/actual.kt
@@ -0,0 +1,2 @@
+package com.jetbrains.handson.mpp.mobile
+actual fun platformName(): String { return "Android" }
---
+++ AndroidIOSKotlinNative/app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.jetbrains.handson.mpp.mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        main_text.text = createApplicationScreenMessage()
+    }
+}
